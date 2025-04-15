ca05957b97376286a6837f06271320f1250530ad
YoungHyun Bae <qodudgus0220@naver.com>
2025-04-15 18:08:26 +0900

    feat: 푸터 구현 및 global css에 폰트 적용

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,40 @@ import styles from '@/app/styles/footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2025 Kia Corporation. All rights reserved.</p>
+      <div className={styles.inner}>
+        <div className={styles.information}>
+          <span className={styles.util}>
+            <button type="button">
+              <b>개인정보 처리방침</b>
+            </button>
+            <button type="button">이용약관</button>
+          </span>
+          <address className={styles.address}>
+            <span>
+              서울특별시 서초구 헌릉로 12 <em>기아㈜</em>
+            </span>
+            <span>
+              대표:&nbsp;<i>송호성, 최준영</i>
+            </span>
+            <span>
+              사업자등록번호:&nbsp;<i>119-81-02316</i>
+            </span>
+            <span>
+              통신판매번호:&nbsp;<i>2006-07935</i>
+            </span>
+            <span>
+              고객센터:&nbsp;<i>1833-4964</i>
+            </span>
+            <span>
+              제휴문의:&nbsp;<a href="mailto:kiabiz@kia.com">kiabiz@kia.com</a>
+            </span>
+          </address>
+        </div>
+        <p className={styles.copyright}>
+          © 2023 KIA CORP. All
+          <br /> Rights Reserved.
+        </p>
+      </div>
     </footer>
   );
 }
